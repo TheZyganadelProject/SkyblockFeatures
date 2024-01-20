dbc780608c8fa42ff7e1a656fce95a76dc658336
Zyganadel <121130526+Zyganadel@users.noreply.github.com>
2024-01-20 13:54:55 -0400

    Data Loading

--- src/main/java/mrfast/sbf/features/dungeons/DoNotInvite.java
+++ src/main/java/mrfast/sbf/features/dungeons/DoNotInvite.java
@@ -1,10 +1,11 @@
 package mrfast.sbf.features.dungeons;
 
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import mrfast.sbf.SkyblockFeatures;
-import mrfast.sbf.utils.Utils;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-
 import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class DoNotInvite {
 
@@ -13,7 +14,29 @@ public class DoNotInvite {
     // Use a debug boolean to enable debug features.
     boolean debug = true;
 
+    //Data things
+TZPFileHandler fileHandler = new TZPFileHandler();
+JsonObject DNI = fileHandler.loadDNI();
+
+}
+
+// Hide data bs behind another class because I don't want to look at this bs unless I absolutely have to.
+class TZPFileHandler{
     // The following should be a temporary file management system until a proper one can be created.
     File TZPDir = new File(SkyblockFeatures.modDir, "TZP");
     File DNIFile = new File(TZPDir, "doNotInvite.json");
+
+public JsonObject loadDNI(){
+return loadData(DNIFile);
+}
+private JsonObject loadData(File dataFile){
+try{
+    String read = new String(Files.readAllBytes(Paths.get(dataFile.getPath())));
+    return new JsonParser().parse(read).getAsJsonObject();
+}
+catch (Exception e){
+    e.printStackTrace();
+    return  null;
+}
+}
 }
