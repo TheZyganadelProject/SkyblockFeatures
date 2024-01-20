1741b751080fc631a943689618aa8bc074288276
Zyganadel <121130526+Zyganadel@users.noreply.github.com>
2024-01-20 15:12:09 -0400

    Add clowns

--- src/main/java/mrfast/sbf/features/dungeons/DoNotInvite.java
+++ src/main/java/mrfast/sbf/features/dungeons/DoNotInvite.java
@@ -3,11 +3,13 @@ package mrfast.sbf.features.dungeons;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import mrfast.sbf.SkyblockFeatures;
+
 import java.io.File;
 import java.io.FileWriter;
-import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.time.Instant;
+import java.util.Date;
 
 public class DoNotInvite {
 
@@ -16,10 +18,23 @@ public class DoNotInvite {
     // Use a debug boolean to enable debug features.
     boolean debug = true;
 
+
     //Data things
     TZPFileHandler fileHandler = new TZPFileHandler();
+    JsonObject config;
     JsonObject DNI = fileHandler.loadDNI();
 
+    void AddClown(String name){
+        // First, get the time.
+        Date date = Date.from(Instant.now());
+
+        // Set the expiry time.
+        long expiryTime = date.getTime();
+        long clownDuration = config.get("clownDuration").getAsLong();
+        expiryTime += clownDuration;
+
+        DNI.addProperty(name, expiryTime);
+    }
 }
 
 // Hide data bs behind another class because I don't want to look at this bs unless I absolutely have to.
@@ -27,14 +42,19 @@ class TZPFileHandler{
     // The following should be a temporary file management system until a proper one can be created.
     File TZPDir = new File(SkyblockFeatures.modDir, "TZP");
     File DNIFile = new File(TZPDir, "doNotInvite.json");
+    File configFile = new File(TZPDir, "config.json");
 
-public JsonObject loadDNI(){
-return loadData(DNIFile);
-}
-public void saveDNI(JsonObject DNI){saveData(DNI, DNIFile);}
+    // DNI things
+    public JsonObject loadDNI(){
+        return loadData(DNIFile);
+    }
+    public void saveDNI(JsonObject DNI){saveData(DNI, DNIFile);}
+    // Config things
+    public JsonObject loadConfig(){return  loadData(configFile);}
+    public void saveConfig(JsonObject config){saveData(config, configFile);}
 
-// Load/Save things.
-private JsonObject loadData(File dataFile){
+    // Load/Save things.
+    private JsonObject loadData(File dataFile){
 try{
     String read = new String(Files.readAllBytes(Paths.get(dataFile.getPath())));
     return new JsonParser().parse(read).getAsJsonObject();
@@ -44,7 +64,7 @@ catch (Exception e){
     return  null;
 }
 }
-private void saveData(JsonObject data, File dataFile) {
+    private void saveData(JsonObject data, File dataFile) {
     String saveThis = data.getAsString();
     try (FileWriter writer = new FileWriter(dataFile)) {
         writer.write(saveThis);
